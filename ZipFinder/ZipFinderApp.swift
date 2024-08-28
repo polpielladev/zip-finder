e7210e61a3c019aeff94e52603d03c64207f4ab4
Pol Piella Abadia <hi@polpiella.dev>
2024-08-28 16:47:35 +0200

    Pushes

---
+++ ZipFinder/ZipFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZipFinderApp.swift
+//  ZipFinder
+//
+//  Created by Pol Piella Abadia on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ZipFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
