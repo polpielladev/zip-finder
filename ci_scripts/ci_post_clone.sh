b5b053f952fb119155d6799d56537b12b3391e59
Pol Piella Abadia <hi@polpiella.dev>
2024-08-31 21:39:32 +0200

    Updates `Fastfile`

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-brew install rbenv
-eval "$(rbenv init - --no-rehash zsh)"
-rbenv install 3.1.1
-rbenv local 3.1.1
+if [ "$CI_WORKFLOW" == "Release iOS" ]; then
+    brew install rbenv
+    eval "$(rbenv init - --no-rehash zsh)"
+    rbenv install 3.1.1
+    rbenv local 3.1.1
+    bundle install
+    bundle exec fastlane update_version_number_if_needed
+fi
 
-bundle install
