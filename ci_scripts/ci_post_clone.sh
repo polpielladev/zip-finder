dfe269327a2d273510a290d9f9fe6f2c4aba0565
Pol Piella Abadia <hi@polpiella.dev>
2024-09-02 17:07:26 +0100

    Create a `post_clone` script

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd ..
+
+if [ "$CI_WORKFLOW" == "Release iOS" ]; then
+    brew install rbenv
+    eval "$(rbenv init - --no-rehash zsh)"
+    rbenv install 3.1.1
+    rbenv local 3.1.1
+
+    bundle install
+    bundle exec fastlane update_version_number_if_needed
+fi
