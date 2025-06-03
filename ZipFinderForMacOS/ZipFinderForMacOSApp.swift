9e5d7e4ed3aa717320fbd214a01f1cba5f2bcda0
Pol Piella Abadia <hi@polpiella.dev>
2025-06-03 16:49:21 +0100

    Creates a new macOS target

---
+++ ZipFinderForMacOS/ZipFinderForMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZipFinderForMacOSApp.swift
+//  ZipFinderForMacOS
+//
+//  Created by Pol Piella Abadia on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ZipFinderForMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
